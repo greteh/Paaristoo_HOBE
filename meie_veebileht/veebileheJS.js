3a2caa07e58ff05270a1ba344a1c7651d0b2e4a7
Grete Hollo <greteh@ut.ee>
2020-11-29 21:22:57 +0200

    Errorite parandus 1: align

--- meie_veebileht/veebileheJS.js
+++ meie_veebileht/veebileheJS.js
@@ -1,9 +1,9 @@
-function naita(id1,id2,id3){
-    document.getElementById(id1).style.display='none'
-    document.getElementById(id2).style.display='block'
-    document.getElementById(id3).style.display='block'
+function naita(id1,id2,id3){ // funktsioon naita votab kolm argumenti, milleks on id.
+    document.getElementById(id1).style.display='none'   // muudab elemendi id-ga id1 nähtamatuks
+    document.getElementById(id2).style.display='block'  // muudab elemendi id-ga id2 nähtavaks
+    document.getElementById(id3).style.display='block'  // muudab elemendi id-ga id3 nähtavaks
 }
-function peida(id1,id2,id3){
+function peida(id1,id2,id3){ // suhteliselt sama mis funktsioon naita
     document.getElementById(id1).style.display='none'
     document.getElementById(id2).style.display='block'
     document.getElementById(id3).style.display='none'
