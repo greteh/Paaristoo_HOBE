1e7ee3b562ce45100ad6efd5b87332c6d0a71b29
Grete Hollo <greteh@ut.ee>
2020-11-29 20:39:55 +0200

    JS tehtud

--- meie_veebileht/veebileheJS.js
+++ meie_veebileht/veebileheJS.js
@@ -3,13 +3,8 @@ function naita(id1,id2,id3){
     document.getElementById(id2).style.display='block'
     document.getElementById(id3).style.display='block'
 }
-function peida(mis){
+function peida(id1,id2,id3){
     document.getElementById(id1).style.display='none'
     document.getElementById(id2).style.display='block'
     document.getElementById(id3).style.display='none'
-}
-function kuinaeb(id1){
-    if (id1.display==true){
-        document.getElementById(id1).style.display='block'
-    }
 }
